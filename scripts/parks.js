e0374e2baf28930c107c2c7a1c2dfde5f9532197
Jonathan <jongroberg@gmail.com>
2023-11-15 10:56:22 -0500

    Add filter functions to parks.js

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,2 +1,16 @@
-import * as data from './data'
-console.log(data.locationsArray)
+import * as data from "./data";
+
+const inputElems = {}
+const outputElems = {}
+
+function filterByPropEquals(array, property, value) {
+  return array.filter((state) => state[property] === value);
+}
+function filterByPropIncludes(array, property, value) {
+  return array.filter((state) => state[property].includes(value));
+}
+
+console.log(filterByPropEquals(data.nationalParksArray, "State", "Utah"));
+console.log(
+  filterByPropIncludes(data.nationalParksArray, "LocationName", "National Park")
+);
