b60d29323621f07abec393e591106a4895d0245e
Jonathan <jongroberg@gmail.com>
2023-11-15 18:27:11 -0500

    Update data and styles files

--- scripts/data.js
+++ scripts/data.js
@@ -1,5 +1,5 @@
 // exports all data arrays to avoid importing each one individually in each script
-export {locationsArray} from "./data_scripts/locationData"
-export {mountainsArray} from './data_scripts/mountainData'
-export {nationalParksArray} from './data_scripts/nationalParkData'
-export {parkTypesArray} from "./data_scripts/parkTypeData"
+export {locationsArray} from "./data_scripts/locationData.js"
+export {mountainsArray} from './data_scripts/mountainData.js'
+export {nationalParksArray} from './data_scripts/nationalParkData.js'
+export {parkTypesArray} from "./data_scripts/parkTypeData.js"
--- scripts/parks.js
+++ scripts/parks.js
@@ -1,16 +1,100 @@
-import * as data from "./data"; // imports all data arrays from data.js
+import * as data from "./data.js"; // imports all data arrays from data.js
 
-const inputElems = {}
-const outputElems = {}
+const inputElems = {};
+const outputElems = { cards: document.getElementById("cards") };
 
+// filtering methods
 function filterByPropEquals(array, property, value) {
   return array.filter((state) => state[property] === value);
 }
 function filterByPropIncludes(array, property, value) {
   return array.filter((state) => state[property].includes(value));
 }
+// card generation
+function generateParkCard(park) {
+  //   const maybePhone = park.Phone
+  //     ? `<label class="card-text small" name="phone" readonly>${park.Phone}</label>`
+  //     : "";
 
-console.log(filterByPropEquals(data.nationalParksArray, "State", "Utah"));
-console.log(
-  filterByPropIncludes(data.nationalParksArray, "LocationName", "National Park")
+  //   function generateCard(title, titleValue, description, descriptionValue) {
+  //     const maybeTitle =
+  //       title !== ""
+  //         ? `label class="card-title w-100" for="${title.toLowerCase()}">${titleValue}</label>`
+  //         : "";
+  //     const maybeDescription =
+  //       description !== ""
+  //         ? `<label class="card-text small" name="${title.toLowerCase()}" readonly>${descriptionValue}</label>`
+  //         : "";
+  //     let card = `<div class="card">
+  //                         <div class="card-body">
+  //                         ${maybeTitle}
+  //                         ${maybeDescription}
+  //                         </div>
+  //                 </div>
+  //                 `;
+  //   }
+  const maybeVisit = park.Visit ? park.Visit : "N/A";
+  return `<div id="${park.LocationID.toLowerCase()}" class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+    <div class="card-group-vertical text-center">
+      <div class="card">
+        <img
+          class="card-img-top"
+          src="./media/images/${
+            data.mountainsArray[park.LocationName.length].img
+          }"
+        />
+      </div>
+      <div class="card">
+        <div class="card-body pb-0">
+          <h5 class="card-title">${park.LocationName}</h5>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <label class="card-title w-100" for=""></label>
+          <label class="card-text small" name="street" readonly
+            >${park.Address}</label
+          >
+          <label class="card-text small" name="cityStateZip" readonly
+            >${park.City}, ${park.State} ${park.ZipCode}</label
+          >
+        </div>
+      </div>
+      <div class="card-group">
+        <div class="card">
+          <div class="card-body ">
+            <label class="card-title w-100" for="phone">Phone</label>
+            <label class="card-text small" name="phone" readonly
+              >${park.Phone == 0 ? "N/A" : park.Phone}</label
+            >
+          </div>
+        </div>
+        <div class="card ">
+          <div class="card-body ">
+            <label class="card-title w-100" for="fax">Fax</label>
+            <label class="card-text small" name="fax" readonly
+              >${park.Fax == 0 ? "N/A" : park.Fax}</label
+            >
+          </div>
+        </div>
+        </div>
+        <div class="card ">
+          <div class="card-body">
+            <label class="card-text w-100 h-100" name="website" readonly>
+              <a class="btn btn-light w-100" href="${maybeVisit}">Learn More</a>
+            </label>
+          </div>
+      </div>
+    </div>
+  </div>`;
+}
+
+const cards = filterByPropEquals(data.nationalParksArray, "State", "Colorado").map(
+  (park) => generateParkCard(park)
 );
+outputElems.cards.innerHTML = cards.join("");
+
+// console.log(filterByPropEquals(data.nationalParksArray, "State", "Utah"));
+// console.log(
+//   filterByPropIncludes(data.nationalParksArray, "LocationName", "National Park")
+// );
